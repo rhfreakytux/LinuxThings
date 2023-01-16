e6242e1262a9e145e0bf6d632f34a16974128169
Narayan Pun Magar <76813953+freakytux@users.noreply.github.com>
2023-01-16 16:37:33 +0545

    Add files via upload

---
+++ tang-key-rotate.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Author: Narayan Pun Magar
+# Date: 01/16/2023
+# Description: Rotating Tang Server Keys and Removing Old keys
+
+TANG_DB=/var/db/tang
+
+cd $TANG_DB
+
+for i in *; do
+    rm $i; done
+    
+/usr/libexec/tangd-keygen $TANG_DB
